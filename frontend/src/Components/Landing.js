ed742a5de3ed101caf0db470bd1776005baa23c2
akd2yf <85966328+andreas-disco@users.noreply.github.com>
2024-11-04 16:48:53 -0500

    hover implemented + font

--- frontend/src/Components/Landing.js
+++ frontend/src/Components/Landing.js
@@ -1,11 +1,17 @@
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 import computer from "../Assets/Objects/Computer.png";
 import mobile from "../Assets/Objects/Mobile.png";
+import computerGif from "../Assets/Objects/Computer.gif";
+import mobileGif from "../Assets/Objects/Mobile.gif";
 
 import "../Styles/Landing.css";
 
 function Landing() {
+    const [isHoveringComputer, setIsHoveringComputer] = useState(false);
+    const [isHoveringMobile, setIsHoveringMobile] = useState(false);
+
     const navigate = useNavigate();
 
     const enter = (isMobile) => {
@@ -15,11 +21,25 @@ function Landing() {
 
     return (
         <div className="OptionContainer">
-            <button onClick={(() => enter(false))} className="OptionButton">
-                <img src={computer} className="OptionIcon"/>
+            <button onClick={(() => enter(false))}
+                onMouseEnter={() => setIsHoveringComputer(true)}
+                onMouseLeave={() => setIsHoveringComputer(false)}
+            className="OptionButton">
+                { isHoveringComputer ? 
+                    <img src={computerGif} className="OptionIcon"/> :
+                    <img src={computer} className="OptionIcon"/>
+                }
+                <h1>Computer</h1>
             </button>
-            <button onClick={(() => enter(true))} className="OptionButton">
-                <img src={mobile} className="OptionIcon"/>
+            <button onClick={(() => enter(true))}
+                onMouseEnter={() => setIsHoveringMobile(true)}
+                onMouseLeave={() => setIsHoveringMobile(false)}
+                className="OptionButton">                
+                { isHoveringMobile ? 
+                    <img src={mobileGif} className="OptionIcon"/> :
+                    <img src={mobile} className="OptionIcon"/>
+                }
+                <h1>Mobile</h1>
             </button>
         </div>
     )
