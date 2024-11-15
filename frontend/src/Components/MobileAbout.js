2436c9ce19b394c2e94bf1c8b6f6eede850b621b
akd2yf <85966328+andreas-disco@users.noreply.github.com>
2024-11-14 19:36:00 -0500

    mobile about setup

--- frontend/src/Components/MobileAbout.js
+++ frontend/src/Components/MobileAbout.js
@@ -1,8 +1,13 @@
+import "../Styles/MobileAbout.css";
+
 function MobileAbout() {
 
     return (
-        <div>
-
+        <div className="MobileAboutContainer">
+            <div className="MobileAboutTitle">FLOAT AND FRIENDS</div>
+            <div className="MobileAboutTagline">A virtual third space connecting you to your next artistic internet best friend</div>
+            <div>Our mission is to create a welcoming virtual third-space for anyone with an idea. Our goal is to foster community, provide resources, and support your ideas from start to finish.</div>
+            <div>Through workshops, socials, and structured [programs], our focus is on artmaking as a communal practice. In a world continuously pushing us to monetize our art, and squishing that art into “content,” FLOAT AND FRIENDS aims to remind us that art can be done for art’s sake, and creativity is in our very human nature. At the same time, we provide workshops, guides, speaker nights, portfolio building opportunities, and other resources to help reveal often hidden steps for those who are interested in a creative career. </div>
         </div>
     );
 }
